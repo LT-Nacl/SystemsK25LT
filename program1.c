0db9e825084632aa1a21737a4a6e0be44b3b8039
LT_Nacl <ltitievsky60@stuy.edu>
2025-09-09 15:28:55 -0400

    playing with data types and printf classwork

--- program1.c
+++ program1.c
@@ -1,4 +1,12 @@
+#include <stdio.h>
 int main(){
-      printf("Hello world.");
-	return 0;    
+      int NumberFour = 4;
+      int* FourPtr = &NumberFour;
+      printf("Hello world.\n");
+      printf("This is the number four: %d \n", NumberFour);
+      printf("The int used to pass four is stored at: %p \n", FourPtr);
+      printf("Here is that location displayed improperly (double): %lf \n", FourPtr);
+      printf("Here is K's funny number in UPPER  hex: %X \n", -559038737);
+      printf("SEGFAULT!!!!: %s", NumberFour);
+      return 0;    
 }
